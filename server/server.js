1c9cca5db3b53f906a04ca304cfdfb5dd8aef3f2
brett <brett_matsumoto@yahoo.com>
2020-07-22 18:26:10 -1000

    added boiler plate express code with successful smoke test

---
+++ server/server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const port = process.env.EXPRESS_CONTAINER_PORT;
+
+const app = express();
+
+app.use('/', (req, res) => {
+    res.send('Hello World')
+});
+
+app.listen(port, () => {
+    console.log(`Express server is listening on Port ${port}.`);
+});
