6ab2835ec67aa861a33144ff9b13957fa130351e
brett <brett_matsumoto@yahoo.com>
2020-07-27 16:39:19 -1000

    created models linking relationships between migrations

---
+++ database/models/Role.js
@@ -0,0 +1,19 @@
+const bookshelf = require('../bookshelf');
+
+require('./User');
+
+class Role extends bookshelf.Model {
+    get tableName() {
+        return 'roles';
+    }
+
+    get hasTimestamps() {
+        return true;
+    }
+
+    users() {
+        return this.hasMany('Users', 'user_id');
+    }
+}
+
+module.exports = bookshelf.model('Role', Role);
